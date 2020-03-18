0fa90a5e70bef069a77178883767d82127530f4b
sacheng <13slcheng@gmail.com>
2020-03-17 22:07:16 -0400

    Add sidebar

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 var express = require('express');
 var mysql = require('mysql');
 var http = require('http');
+var path = require('path');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', function (req, res) {
     //res.send('Hello World');
-    res.render('pages/index');
+    res.render('pages/dashboard');
 });
 
-app.get('/about', function (req, res) {
-    res.render('pages/about');
+app.get('/statistics', function (req, res) {
+    res.render('pages/statistics');
+});
+
+app.get('/reservations', function (req, res) {
+    res.render('pages/reservations');
+});
+
+app.get('/customers', function (req, res) {
+    res.render('pages/customers');
 });
 
 var db = mysql.createConnection({
@@ -23,14 +34,14 @@ var db = mysql.createConnection({
     //port:
 });
 
-db.connect( function (err) {
-    if ( err ) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
+// db.connect( function (err) {
+//     if ( err ) {
+//         console.error('error connecting: ' + err.stack);
+//         return;
+//     }
 
-    console.log('connected as id ' + db.threadId);
-})
+//     console.log('connected as id ' + db.threadId);
+// })
 
 // var server = app.listen(8081, function () {
 //     var host = server.address().address;
