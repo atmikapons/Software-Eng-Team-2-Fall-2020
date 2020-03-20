872b8ae8428fc88ee258584ec5ea6277ccb452d3
sacheng <13slcheng@gmail.com>
2020-03-20 02:21:24 -0400

    Add route to add new customer

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
-var express = require('express');
+const express = require('express');
+
 var router = function (app, db) {
     ////// GET WEBPAGES //////////
     app.get('/', function (req, res) {
@@ -27,7 +28,7 @@ var router = function (app, db) {
         });
     });
 
-    //////////////////////////////
+    ////// CUSTOMER ROUTES //////
 
     app.get('/deleteCustomer/:barcode', function (req, res) {
         let id = req.params.barcode;
@@ -40,6 +41,37 @@ var router = function (app, db) {
         });
     });
 
+    app.post('/addCustomer', function (req, res) {
+        let first = req.body.first;
+        let last = req.body.last;
+        let email = req.body.email;
+        let phone = req.body.phone;
+        let pass = req.body.password;
+        let barcode = req.body.barcode;
+        let license = req.body.license;
+        let reg = req.body.reg;
+        let cctype = req.body.cctype;
+        let ccnum = req.body.ccnum;
+        let ccexp = req.body.ccexp;
+        let cccvv = req.body.cccvv;
+        let points = req.body.points;
+        let handicap = req.body.handicap;
+        let values = [];
+        values.push([first, last, email, phone, pass, barcode, license, reg,
+            cctype, ccnum, ccexp, cccvv, points, handicap]);
+        let addQuery = "INSERT INTO CustomerInfo (FirstName, LastName, Email, PhoneNum, \
+                        Password, Barcode, LicenseNum, RegistrationNum, CreditCardType, \
+                        CreditCardNum, ExpDate, CVV, Points, Handicap) VALUES ?;";
+        db.query(addQuery, [values], function (err, result) {
+            if ( err ) {
+                return res.status(500).send(err);
+            } else {
+                return res.status(200).send({
+                    'status': "Success"
+                });
+            }
+        });
+    });
 };
 
 module.exports = router;
