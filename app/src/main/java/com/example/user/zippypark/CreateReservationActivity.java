be856745e1ba78430ea91780542ee3eadc6492ea
Atmika-Ponnusamy <atmikapons@gmail.com>
2020-03-19 20:39:54 -0400

    DB access and added .java files for all activities

---
+++ app/src/main/java/com/example/user/zippypark/CreateReservationActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.zippypark;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 3/19/2020.
+ */
+
+public class CreateReservationActivity extends AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_reservation);
+    }
+}
