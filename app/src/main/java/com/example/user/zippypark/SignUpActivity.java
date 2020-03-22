f952fe2a2daf6013ecf7fe456c4243114999483d
smoy99 <moy.samantha@outlook.com>
2020-03-21 20:44:34 -0400

    Implements Sign Up functionality and bi-directional navigation between MainActivity and Sign Up. Adds cancel button to Sign Up.

--- app/src/main/java/com/example/user/zippypark/SignUpActivity.java
+++ app/src/main/java/com/example/user/zippypark/SignUpActivity.java
@@ -6,18 +6,45 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.jcraft.jsch.*;
+
+import java.sql.*;
+
 
 /**
  * Created by user on 3/19/2020.
  */
 
 public class SignUpActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    Button nxt;
+    Button cancel;
+    EditText firstNameEditText;
+    EditText lastNameEditText;
+    EditText emailEditText;
+    EditText phoneEditText;
+    EditText passwordEditText;
+    EditText licenseNumEditText;
+    EditText registrationNumEditText;
+    EditText creditCardTypeEditText;
+    EditText creditCardNumEditText;
+    EditText expDateEditText;
+    EditText cvvEditText;
+    //Spinner handicapEditText;
+    static Connection conn;
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
 
-        //Handicap Spinner
-        Spinner spinner = (Spinner) findViewById(R.id.handicapSpinner);
+        //HANDICAP SPINNER SETUP
+        final Spinner spinner = (Spinner) findViewById(R.id.handicapSpinner);
         spinner.setOnItemSelectedListener(this);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -27,23 +54,93 @@ public class SignUpActivity extends AppCompatActivity implements AdapterView.OnI
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
-        /*
-        spinner.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        //SIGN UP INFO
+        firstNameEditText = findViewById(R.id.firstNameEditText);
+        lastNameEditText = findViewById(R.id.lastNameEditText);
+        emailEditText = findViewById(R.id.emailEditText);
+        phoneEditText = findViewById(R.id.phoneEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        licenseNumEditText = findViewById(R.id.licenseNumEditText);
+        registrationNumEditText = findViewById(R.id.registrationNumEditText);
+        creditCardTypeEditText = findViewById(R.id.creditCardTypeEditText);
+        creditCardNumEditText = findViewById(R.id.creditCardNumEditText);
+        expDateEditText = findViewById(R.id.expDateEditText);
+        cvvEditText = findViewById(R.id.cvvEditText);
 
+        //NEXT BUTTON
+        nxt = findViewById(R.id.nextButton);
+        nxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String firstName = firstNameEditText.getText().toString();
+                final String lastName = lastNameEditText.getText().toString();
+                final String email = emailEditText.getText().toString();
+                //final int phone = Integer.parseInt(phoneEditText.getText().toString());
+                final String phone = phoneEditText.getText().toString();
+                final String password = passwordEditText.getText().toString();
+                final String licenseNum = licenseNumEditText.getText().toString();
+                final String registrationNum = registrationNumEditText.getText().toString();
+                final String creditCardType = creditCardTypeEditText.getText().toString();
+                final String creditCardNum = creditCardNumEditText.getText().toString();
+                final String expDate = expDateEditText.getText().toString();
+                final String cvv = cvvEditText.getText().toString();
+                final String handicapAns = spinner.getSelectedItem().toString();
+
+                //convert handicap response to number for database
+                int num = 0;
+                if (handicapAns.equals("Yes")) {
+                    num = 1;
+                }
+                final int handicapAnsNum = num;
+
+                AsyncTask.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Statement stmt = MainActivity.conn.createStatement();
+                            //query has no barcode (autoincrement) and user starts with 10 points
+                            String query = "INSERT INTO `CustomerInfo`" +
+                                    "(`FirstName`, `LastName`, `Email`, `PhoneNum`, `Password`, " +
+                                    "`LicenseNum`, `RegistrationNum`, `CreditCardType`, " +
+                                    "`CreditCardNum`, `ExpDate`, `CVV`,`Points`, `Handicapped`) " +
+                                    "VALUES ('" + firstName + "', '" + lastName + "', '" +
+                                    email + "', '" + phone + "', '" + password + "', '" + licenseNum + "', '" +
+                                    registrationNum + "', '" + creditCardType + "', '" + creditCardNum +
+                                    "', '" + expDate + "', '" + cvv + "', '10', '" + handicapAnsNum + "')";
+                            //System.out.println(query);
+                            stmt.executeUpdate(query);
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+                Intent i = new Intent(SignUpActivity.this, HomeMenuActivity.class);
+                startActivity(i);
             }
+
         });
 
-         */
 
+        //CANCEL BUTTON
+        cancel = findViewById(R.id.cancelButton);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+
+        });
 
     }
 
-
+    //SPINNER METHODS (need to be here but do nothing)
     public void onItemSelected(AdapterView<?> parent, View view,
                                int pos, long id) {
         // An item was selected. You can retrieve the selected item using
         // parent.getItemAtPosition(pos)
+
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
