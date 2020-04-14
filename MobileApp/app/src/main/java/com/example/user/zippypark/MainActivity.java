4a0051ce8b0d6c9b9115ef94c6f4af1f01cff64d
smoy99 <moy.samantha@outlook.com>
2020-04-13 22:22:11 -0400

    Adds notification of incorrect login info.

--- MobileApp/app/src/main/java/com/example/user/zippypark/MainActivity.java
+++ MobileApp/app/src/main/java/com/example/user/zippypark/MainActivity.java
@@ -5,10 +5,12 @@ import android.os.AsyncTask;
 import android.support.annotation.UiThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.jcraft.jsch.*;
 
@@ -44,35 +46,50 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String username = userNameEditText.getText().toString();
-                final String password = passwordEditText.getText().toString();
+                if (TextUtils.isEmpty(userNameEditText.getText().toString())) {
+                    userNameEditText.setError("Enter username");
+                } else if (TextUtils.isEmpty(passwordEditText.getText().toString())) {
+                    passwordEditText.setError("Enter password");
+                } else {
+                    final String username = userNameEditText.getText().toString();
+                    final String password = passwordEditText.getText().toString();
 
-                AsyncTask.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            Statement stmt = conn.createStatement();
-                            ResultSet rs = stmt.executeQuery("SELECT Barcode FROM CustomerInfo " +
-                                    "WHERE Email=\"" + username + "\" AND Password=\"" +
-                                    password + "\"");
-                            if(rs.next()) {
-                                int barcode = rs.getInt("Barcode");
-                                Intent i = new Intent(MainActivity.this,
-                                        HomeMenuActivity.class);
-                                Bundle b = new Bundle();
-                                b.putInt("barcode", barcode);
-                                i.putExtras(b);
-                                startActivity(i);
-                            } else {
-                                //figure out how to throw an error to the user
-                                //if the username and/or password is incorrect
-                                //userNameEditText.setError("User does not exist OR incorrect password");
+
+                    AsyncTask.execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                Statement stmt = conn.createStatement();
+                                ResultSet rs = stmt.executeQuery("SELECT Barcode FROM CustomerInfo " +
+                                        "WHERE Email=\"" + username + "\" AND Password=\"" +
+                                        password + "\"");
+                                if (rs.next()) {
+                                    int barcode = rs.getInt("Barcode");
+                                    Intent i = new Intent(MainActivity.this,
+                                            HomeMenuActivity.class);
+                                    Bundle b = new Bundle();
+                                    b.putInt("barcode", barcode);
+                                    i.putExtras(b);
+                                    startActivity(i);
+                                } else {
+                                    //figure out how to throw an error to the user
+                                    //if the username and/or password is incorrect
+                                    //Incorrect info notification
+                                    runOnUiThread(new Runnable(){ //can't toast from non UI threads (this is the workaround)
+                                        public void run(){
+                                            Toast toast = Toast.makeText(getApplicationContext(),
+                                                    "Login information incorrect.", Toast.LENGTH_LONG);
+                                            toast.show();
+                                        }
+                                    });
+
+                                }
+                            } catch (SQLException e) {
+                                e.printStackTrace();
                             }
-                        } catch (SQLException e) {
-                            e.printStackTrace();
                         }
-                    }
-                });
+                    });
+                }
             }
         });
 
@@ -106,14 +123,14 @@ public class MainActivity extends AppCompatActivity {
             session.setConfig("StrictHostKeyChecking", "no");
             Log.d("MainActivity", "Establishing Connection...");
             session.connect();
-            Log.d("MainActivity","Connection established.");
+            Log.d("MainActivity", "Connection established.");
 
             int assigned_port = session.setPortForwardingL(0, "localhost", rport);
-            Log.d("MainActivity","localhost:"+assigned_port+" -> "+rhost+":"+rport);
-            Log.d("MainActivity","Port Forwarded");
+            Log.d("MainActivity", "localhost:" + assigned_port + " -> " + rhost + ":" + rport);
+            Log.d("MainActivity", "Port Forwarded");
             //Class.forName(driverName);
             conn = DriverManager.getConnection("jdbc:mysql://localhost:" + assigned_port + "/zippypark?user=" + dbUsername + "&password=" + dbPassword + "&useUnicode=true&characterEncoding=UTF-8");
-        }catch (JSchException e) {
+        } catch (JSchException e) {
             e.printStackTrace();
         }//        finally {
 //            if(conn != null && !conn.isClosed()){
