7020085e0cec4a5ce8104d5b27705dffdfb4bff1
sacheng <13slcheng@gmail.com>
2020-04-22 23:43:11 -0400

    Add automated testing for all routes, remove models

--- Website/server.js
+++ Website/server.js
@@ -13,38 +13,11 @@ app.use(bodyParser.json()); // parse form data client
 app.use(express.static(path.join(__dirname, 'public'))); // configure express to use public folder
 
 // // database connection
-// var db = mysql.createConnection({
-//     host: '127.0.0.1', // local host
-//     user: 'zippypark',
-//     password: 'goyhVynIiLcJgHpN',
-//     database: 'zippypark'
-// });
-
-// Kylie's remote database for testing:
-// const db = mysql.createConnection({
-//     host: 'remotemysql.com',
-//     user: 'KUU6GpyPt4',
-//     password: 'XIbBmfeIWj',
-//     database: 'KUU6GpyPt4',
-//     port: 3306
-// });
-
-// // Sam's remote database for testing:
-/*
-const db = mysql.createConnection({
-    host: 'remotemysql.com',
-    user: 'dhI47C5XFR',
-    password: 'R5EYIRUesL',
-    database: 'dhI47C5XFR',
-    port: 3306
-});*/
-// // Nandita's remote database for testing:
-const db = mysql.createConnection({
-    host: 'remotemysql.com',
-    user: '7X8Fr3nKz9',
-    password: 'M8b3pMtr9B',
-    database: '7X8Fr3nKz9',
-    port: 3306
+var db = mysql.createConnection({
+    host: '127.0.0.1', // local host
+    user: 'zippypark',
+    password: 'goyhVynIiLcJgHpN',
+    database: 'zippypark'
 });
 
 db.connect( function (err) {
@@ -52,12 +25,9 @@ db.connect( function (err) {
         console.error('error connecting: ' + err.stack);
         return;
     }
-    //console.log('connected as id ' + db.threadId);
 });
 
 const routes = require('./routes/index.js')(app,db);
 
 const httpServer = http.createServer( app );
 httpServer.listen(8080); // can change port
-
-module.exports = app; // for testing
--- Website/test/test_routes.js
+++ Website/test/test_routes.js
@@ -9,9 +9,30 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('Initializing', () => {
-    it('it should check app status', (done) => {
+describe('Initializing...', () => {  
+    it('check DASHBOARD', (done) => {
         chai.request(server).get('/').end((err, res) => {
+            should.not.exist(err);
+            res.should.have.status(200);
+            done();
+        });
+    });
+    it('check STATISTICS', (done) => {
+        chai.request(server).get('/statistics').end((err, res) => {
+            should.not.exist(err);
+            res.should.have.status(200);
+            done();
+        });
+    });
+    it('check RESERVATIONS', (done) => {
+        chai.request(server).get('/reservations').end((err, res) => {
+            should.not.exist(err);
+            res.should.have.status(200);
+            done();
+        });
+    });
+    it('check CUSTOMERS', (done) => {
+        chai.request(server).get('/customers').end((err, res) => {
             should.not.exist(err);
             res.should.have.status(200);
             done();
@@ -20,95 +41,316 @@ describe('Initializing', () => {
 });
 
 describe('Customer Account', () => {
-    // describe('/ADD customer', () => {
-    //     it('it should ADD a customer account', (done) => {
-    //         const customer = {
-    //             first: 'Sam',
-    //             last: 'Cheng',
-    //             email: 'test@email.com',
-    //             phone:'555-555-5555',
-    //             password: 'password',
-    //             barcode: 31415926535,
-    //             license: '123ABC',
-    //             reg: '1234567',
-    //             cctype: 'Visa',
-    //             ccnum:'1234576578',
-    //             ccexp:'01-24',
-    //             cccvv:'123',
-    //             points:0,
-    //             handicap:0
-    //         }
-    //         chai.request(server)
-    //         .post('/addCustomer')
-    //         .send(customer)
-    //         .end((err, res) => {
-    //             res.should.have.status(200);
-    //             done();
-    //         });
-    //     });
-    // });
-    describe('/DELETE customer', () => {
-        it('it should DELETE a customer given the barcode', (done) => {
-            const customer = {
-                barcode: 31415926535
-            }
-            chai.request(server)
-            .get('/deleteCustomer/' + customer.barcode)
-            .send(customer)
-            .end((err, res) => {
-                res.should.have.status(200);
-                done();
-            });
+    it('ADD a customer account', (done) => {
+        const customer = {
+            first: 'Sam',
+            last: 'Cheng',
+            email: 'test@email.com',
+            phone:'555-555-5555',
+            password: 'password',
+            barcode: '314159',
+            license: '123ABC',
+            reg: '1234567',
+            cctype: 'Visa',
+            ccnum:'1234576578',
+            ccexp:'01-24',
+            cccvv:'123',
+            points:'0',
+            handicap:'0'
+        }
+        chai.request(server)
+        .post('/addCustomer')
+        .send(customer)
+        .end((err, res) => {
+            res.should.have.status(200);
+            done();
+        });
+    });
+    it('UPDATE a customer with an existing account', (done) => {
+        const customer = {
+            first: 'Bob',
+            last: 'Cheng',
+            email: 'bob@email.com',
+            phone:'555-555-5555',
+            password: 'password',
+            barcode: '314159',
+            license: '123ABC',
+            reg: '1234567',
+            cctype: 'Visa',
+            ccnum:'1234576578',
+            ccexp:'01-24',
+            cccvv:'123',
+            points:'0',
+            handicap:'0'
+        }
+        chai.request(server)
+        .post('/editCustomer')
+        .send(customer)
+        .end((err, res) => {
+            res.should.have.status(200);
+            done();
+        });
+    });
+    it('GET an existing customer', (done) => {
+        const customer = {
+            barcode: 314159
+        }
+        chai.request(server)
+        .post('/getCustomer')
+        .send(customer)
+        .end((err, res) => {
+            res.should.have.status(200);
+            done();
+        });
+    });
+    it('DELETE a customer given the barcode', (done) => {
+        const customer = {
+            barcode: 314159
+        }
+        chai.request(server)
+        .get('/deleteCustomer/' + customer.barcode)
+        .end((err, res) => {
+            res.should.have.status(200);
+            done();
+        });
+    });  
+});
+
+describe('Reservations', () => {
+    before((done) => {
+        const customer = {
+            first: 'Sam',
+            last: 'Cheng',
+            email: 'test@email.com',
+            phone:'555-555-5555',
+            password: 'password',
+            barcode: '314159',
+            license: '123ABC',
+            reg: '1234567',
+            cctype: 'Visa',
+            ccnum:'1234576578',
+            ccexp:'01-24',
+            cccvv:'123',
+            points:'0',
+            handicap:'0'
+        }
+        chai.request(server)
+        .post('/addCustomer')
+        .send(customer)
+        .end((err, res) => {
+            res.should.have.status(200);
+            done();
+        });
+    });
+    after((done) => {
+        const customer = {
+            barcode: 314159
+        }
+        chai.request(server)
+        .get('/deleteCustomer/' + customer.barcode)
+        .end((err, res) => {
+            res.should.have.status(200);
+            done();
+        });
+    })
+    it('ADD a reservation for an existing account', (done) => {
+        const reservation = {
+            date: '2020-04-30',
+            start: '10:00',
+            end: '12:00',
+            barcode: '314159',
+            spot: '2',
+            vip: '0',
+            charge: '25.00',
+            rid: '265358979'
+        }
+        chai.request(server)
+        .post('/addReservation')
+        .send(reservation)
+        .end((err, res) => {
+            if (err) return done(err);
+            res.should.have.status(200);
+            done();
+        });
+    });
+    it('GET a reservation given the reservation id', (done) => {
+        const reservation = {
+            rid: '265358979'
+        }
+        chai.request(server)
+        .post('/getReservation')
+        .send(reservation)
+        .end((err, res) => {
+            res.should.have.status(200);
+            done();
+        });
+    });
+    it('UPDATE a reservation given the reservation id', (done) => {
+        const reservation = {
+            date: '2020-05-30',
+            start: '10:00',
+            end: '12:00',
+            barcode: '314159',
+            spot: '2',
+            vip: '0',
+            charge: '30.00',
+            rid: '265358979'
+        }
+        chai.request(server)
+        .post('/editReservation')
+        .send(reservation)
+        .end((err, res) => {
+            res.should.have.status(200);
+            done();
+        });
+    });
+    it('DELETE a reservation given the reservation id', (done) => {
+        const reservation = {
+            rid: '265358979'
+        }
+        chai.request(server)
+        .get('/deleteReservation/' + reservation.rid)
+        .end((err, res) => {
+            res.should.have.status(200);
+            done();
         });
     });
 });
 
-describe('Reservation', () => {
-    describe('/GET reservation', () => {
-        it('it should GET a reservation given the id', (done) => {
-            const reservation = {
-                rid: 134569
-            }
-            chai.request(server)
-            .post('/getReservation')
-            .send(reservation)
-            .end((err, res) => {
-                res.should.have.status(200);
-                done();
-            });
+describe('Statistics', () => {  
+    it('GET reservations by date over last 30 days', (done) => {
+        chai.request(server).get('/byDate').end((err, res) => {
+            should.not.exist(err);
+            res.should.have.status(200);
+            done();
         });
     });
-    describe('/EDIT reservation', () => {
-        it('it should EDIT a reservation given the id', (done) => {
-            const reservation = {
-                date: '2020-03-21',
-                start: '11:00',
-                end: '23:00',
-                barcode: 2345678,
-                spot: -1,
-                charge: 160,
-                rid: 134569,
-            }
-            chai.request(server)
-            .post('/editReservation')
-            .send(reservation)
-            .end((err, res) => {
-                res.should.have.status(200);
-                done();
-            });
+    it('GET walk-ins by date over last 30 days', (done) => {
+        chai.request(server).get('/byDateWI').end((err, res) => {
+            should.not.exist(err);
+            res.should.have.status(200);
+            done();
         });
     });
-    // describe('/DELETE/:rid reservation', () => {
-    //     it('it should DELETE the reservation given the rid', (done) => {
-    //         const reservation = {
-    //             rid: 23145
-    //         }
-    //         chai.request(server)
-    //         .get('/deleteReservation/' + reservation.id)
-    //         .end((err, res) => {
-    //             res.should.have.status(200);
-    //             done();
-    //         });
-    //     });
-    // });
+    it('GET average reservations per hour over last 30 days', (done) => {
+        chai.request(server).get('/byTime').end((err, res) => {
+            should.not.exist(err);
+            res.should.have.status(200);
+            done();
+        });
+    });
+    it('GET average reservations per hour over last 30 days', (done) => {
+        chai.request(server).get('/byTimeWI').end((err, res) => {
+            should.not.exist(err);
+            res.should.have.status(200);
+            done();
+        });
+    });
+    it('GET revenue per day over the last week', (done) => {
+        chai.request(server).get('/revWeek').end((err, res) => {
+            should.not.exist(err);
+            res.should.have.status(200);
+            done();
+        });
+    });
+    it('GET average revenue per hour over last 30 days', (done) => {
+        chai.request(server).get('/revPerHour').end((err, res) => {
+            should.not.exist(err);
+            res.should.have.status(200);
+            done();
+        });
+    });
+});
+
+describe('Dashboard', () => {  
+    it('GET number of open parking spots', (done) => {
+        chai.request(server)
+        .get('/getParkingSpots')
+        .end((err, res) => {
+            res.should.have.status(200);
+            done();
+        });
+    });
+    it('GET number of reservations by date', (done) => {
+        chai.request(server)
+        .get('/getNumReservations')
+        .end((err, res) => {
+            res.should.have.status(200);
+            done();
+        });
+    });
+    it("GET yesterday's revenue", (done) => {
+        chai.request(server)
+        .get('/getRevenue')
+        .end((err, res) => {
+            res.should.have.status(200);
+            done();
+        });
+    });
+    it('GET number of customer accounts', (done) => {
+        chai.request(server)
+        .get('/getNumCustomers')
+        .end((err, res) => {
+            res.should.have.status(200);
+            done();
+        });
+    });
+    it('GET price scheme', (done) => {
+        chai.request(server)
+        .get('/getPriceForm')
+        .end((err, res) => {
+            res.should.have.status(200);
+            done();
+        });
+    });
+    let createRes, createRes30x, onTime, overstay, cancels, exchangeVIP;
+    it('GET points scheme', (done) => {
+        chai.request(server)
+        .get('/getPointsForm')
+        .end((err, res) => {
+            res.should.have.status(200);
+            let data = JSON.parse(res.text).data;
+            createRes = data[0].Points;
+            createRes30x = data[1].Points;
+            onTime = data[2].Points;
+            overstay = data[3].Points;
+            cancels = data[4].Points;
+            exchangeVIP = data[5].Points;
+            done();
+        });
+    });
+    it('UPDATE points scheme', (done) => {
+        const pointScheme = {
+            createRes: createRes,
+            createRes30x: createRes30x,
+            onTime: onTime,
+            overstay: overstay,
+            cancels: cancels,
+            exchangeVIP: -50,
+        }
+        chai.request(server)
+        .post('/editPointsForm')
+        .send(pointScheme)
+        .end((err, res) => {
+            res.should.have.status(200);
+            done();
+        });
+    });
+    after((done) => { // reset to normal
+        const pointScheme = {
+            createRes: createRes,
+            createRes30x: createRes30x,
+            onTime: onTime,
+            overstay: overstay,
+            cancels: cancels,
+            exchangeVIP: exchangeVIP
+        }
+        chai.request(server)
+        .post('/editPointsForm')
+        .send(pointScheme)
+        .end((err, res) => {
+            res.should.have.status(200);
+            done();
+        });
+    })
 });
